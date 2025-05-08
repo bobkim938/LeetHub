7e3d5696fd6d15ab56bbfc6ade6c2294c0f2bcd3
Min Joong Kim <115132363+bobkim938@users.noreply.github.com>
2025-05-08 16:34:12 +0800

    Time: 12 ms (49.14%), Space: 14.1 MB (28.68%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        int threeSumClosest(vector<int>& nums, int target) {
+            sort(nums.begin(), nums.end());
+            int output, diff = INT_MAX;
+            for(int i = 0; i < nums.size() - 2; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) continue; 
+                int left = i + 1;
+                int right = nums.size() - 1;
+                while(left < right) {
+                    int sum = nums[i] + nums[left] + nums[right];
+                    if(abs(target - sum) < abs(diff)) {
+                        diff = target - sum;
+                        output = sum;
+                    }
+                    if(sum < target) {
+                        left++;
+                    }
+                    else right--;
+                }
+            }
+            return output;
+        }
+};
